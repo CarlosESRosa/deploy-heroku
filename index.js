7a44a87ffdf877c311ad6dd95656bdf71003fd98
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-06-02 16:10:57 -0300

    fix lint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 require('dotenv').config();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1>O pai ta on na ${port}</h1>`))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send(`<h1>O pai ta on na ${port}</h1>`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
