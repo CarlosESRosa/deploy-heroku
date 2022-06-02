b38845dbb148f60d70fa2e3825c4eeee3f4a17d5
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-06-02 15:59:45 -0300

    add h1

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(`<h1>O pai ta on na ${port}</h1>`))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
